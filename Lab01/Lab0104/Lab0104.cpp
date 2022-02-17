fb92a67479928e7a2a5b8948506ca6a1791d0c63
Angelina Nianchur <angelinanianchur@gmail.com>
2022-02-17 12:09:49 +0200

    Lab0104

--- Lab01/Lab0104/Lab0104.cpp
+++ Lab01/Lab0104/Lab0104.cpp
@@ -1,7 +1,17 @@
-﻿#include<cmath>
-#include <iostream>
+﻿#include <iostream>
+#include <math.h>
 
 using namespace std;
 int main()
 {
 	setlocale(LC_CTYPE, "ukr");
+	float T, A, Y, S;
+	cout << "Час(год)=";
+	cin >> T;
+	cout << "Прискорення(км/с)=";
+	cin >> A;
+	cout << "швидкість автомобілів(км/год)=";
+	cin >> Y;
+	S = 120 + 2 * Y * T + (2 * A * (T * T) / 4);
+	cout << "Відстань між авто=" << S;
+}
